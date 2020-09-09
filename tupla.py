513a7a60f583621597d8d492f351ee8fc98bc0ff
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-09 14:59:03 -0300

    :umbrella: fix: comandos

--- tupla.py
+++ tupla.py
@@ -1,2 +1,12 @@
 minhaTupla = ("Açai","Nutella","Leite Ninho")
 print("Mostrar a tupla: ", minhaTupla)
+print("Mostrar item em posição específica: ", minhaTupla[0])
+print("Mostrar o último item da tupla: ", minhaTupla[-1])
+print("Mostrar um intervalo de itens: ", minhaTupla[1:2])
+
+listaClandestina = list(minhaTupla)
+print("Lista clandestina: ", listaClandestina)
+listaClandestina[2] = "Leite Condensado"
+print("Lista clandestina com dado alterado: ", listaClandestina)
+
+minhaTupla = tuple(listaClandestina)
